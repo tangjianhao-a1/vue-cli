021474669a66bbb1cfc629fd56cdf0cba6c4de56
ede <837221025@qq.com>
2021-01-11 16:17:38 +0800

    路由代码跳转

--- learn-vue-router/src/App.vue
+++ learn-vue-router/src/App.vue
@@ -1,14 +1,26 @@
 <template>
   <div id="app">
-  <router-link to="/home" tag="button" replace="">首页</router-link>  
+ <!--  <router-link to="/home" tag="button" replace="">首页</router-link>  
   <router-link to="/about" tag="button" replace="">关于</router-link>  
+  <router-view></router-view> -->  
+  <button @click="homeClick">首页</button>  
+  <button @click="aboutClick">关于</button>  
   <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',  
+  methods: {
+    homeClick() {
+      //通过代码的方式修改路由vue-router  
+      this.$router.push('/home')
+    },  
+    aboutClick() {
+      this.$router.push('/about')
+    }
+  }
 }
 </script>
 
