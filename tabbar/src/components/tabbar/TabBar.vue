2c3ac3afc920962ef98ae3d9f199a55ee73010a0
ede <837221025@qq.com>
2021-01-15 15:17:03 +0800

    组件，插槽的使用

---
+++ tabbar/src/components/tabbar/TabBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="tab-bar">
+  <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar"
+};
+</script>
+
+<style>
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+  font-size: 20px;
+  box-shadow: 10px -3px 15px rgba(100, 100, 100, 0.5);
+}
+
+
+</style>
---
+++ tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tab-bar-item">
+  <slot name="item-icon"></slot>
+  <slot name="item-text"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem"
+}
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+}
+</style>
